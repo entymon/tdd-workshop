39e9659b98cf911822ecf6e7822f8e668e8aa27e
entymon <pawel.olejniczak.i@gmail.com>
2020-09-11 13:06:17 +0200

    workshop 2

--- balance-brackets/balance-brackets.js
+++ balance-brackets/balance-brackets.js
@@ -1,5 +1,8 @@
 function balanceBrackets(string) {
-    if(string === '([}]') {
+    if (string === '([}]') {
+        return false
+    }
+    if (string.length % 2 === 1) {
         return false
     }
 
--- balance-brackets/balance-brackets.test.js
+++ balance-brackets/balance-brackets.test.js
@@ -11,3 +11,7 @@ test('([}] is not balanced', () => {
 test('] is not balanced', () => {
   expect(balanceBrackets(']')).toBeFalsy();
 });
+
+test('(] is not balanced', () => {
+  expect(balanceBrackets('(]')).toBeFalsy();
+});
